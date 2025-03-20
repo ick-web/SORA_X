b952e9a1fc09780e70cc31850fe72b1eb234b375
mzsoba <owt4832@gmail.com>
2025-03-20 17:52:58 +0900

    feat/ 프로젝트 초기 셋팅

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ const Header = () => {
         <nav className="container mx-auto flex justify-around">
           <Link href="/">질문하기</Link>
           <Link href="/community">만남의 광장?</Link>
+          <Link href="/ranking">랭킹 보기</Link>
           <Link href="/mypage">마이페이지</Link>
           <Link href="/login">로그인 만들어주세요</Link>
         </nav>
