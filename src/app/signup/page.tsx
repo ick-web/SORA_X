e814f7f2ab641be9274f602b0af6d7d85701e62c
ick-web <yih000098@gmail.com>
2025-03-21 20:35:41 +0900

    🎉 add: 소라고둥 이미지 컴포넌트 추가

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,11 @@
 import AuthForm from "@/components/(login,signup)/Form";
+import Sora from "@/components/(login,signup)/Sora";
 import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div className="flex flex-row justify-center items-center bg-black ">
+    <div className="flex flex-row justify-evenly p-8 items-center bg-black">
+      <Sora />
       <AuthForm mode="signup" />
     </div>
   );
