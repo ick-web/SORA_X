ec040affcb22fcbd250d3c928976e4af5efb5cbf
siheon <rlatlgjs357@gmail.com>
2025-03-21 17:28:20 +0900

    ✨feat/�style / 기본 UI 구성 & 댓글 입력 창 구현

---
+++ src/components/detail/QuestionDetail.tsx
@@ -0,0 +1,12 @@
+const QuestionDetail = () => {
+  return (
+    <div className="p-4 mb-10 border-b border-gray-700">
+      <h2 className="text-2xl font-bold mb-4">질문</h2>
+      <p className="bg-gray-700 mb-6 rounded p-2">1+1이 왜 2인가요? 저는 이해가 안 돼요.</p>
+      <h2 className="text-2xl font-bold mb-4">풀이</h2>
+      <p className="bg-gray-700 mb-6 rounded p-2">1+1이 2가 되는 이유는 기본적인 수학 개념 때문입니다...</p>
+    </div>
+  );
+};
+
+export default QuestionDetail;
