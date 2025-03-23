2d7e837920ceaeed1dda5c17d8116752b1fb2b5d
usuny0317 <usuny0317@naver.com>
2025-03-24 02:27:19 +0900

    ✨ feat/ 주스탄드를 사용하여 유저정보를 유지한다는 기준으로 유저 게시글 가져오기기

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,12 +1,45 @@
-import Mycard from "@/components/(mypage)/Mycard";
+import Myanswercard from "@/components/(mypage)/Myanswercard";
+import supabase from "../supabase/client";
 
-const MyPage = () => {
+//후에 주스탄드로 유저 정보 저장 및 가져올 것
+//user의 Nickname도 함께 저장 되어있으면 좋겠습니다.
+const user = {
+  id: "6945ddb3-1281-4b59-b16c-f238709e37a9",
+  nickname: "수수수",
+};
+
+const SUPABASE_TABLE_NAME = {
+  ANSWER: "answers",
+  COMMENTS: "comments",
+};
+
+const MyPage = async () => {
+  //게시글 불러오는 함수 (후에 커리에 넣을 예정)
+  const getuserpost = async (userid: string) => {
+    const { data, error } = await supabase
+      .from(SUPABASE_TABLE_NAME.ANSWER)
+      .select(
+        `
+      *,
+      users(*)
+    `
+      )
+      .eq("answer_user_id", userid);
+    if (error) {
+      throw error;
+    }
+    return data;
+  };
+
+  const a = await getuserpost(user.id);
+
   return (
     <div>
       <hr className="border-t border-gray-400 my-6" />
       <div className="flex flex-col  ">
-        <Mycard />
-        <Mycard />
+        {a?.map((aa) => {
+          return <Myanswercard {...aa} key={aa.answer_id} />;
+        })}
       </div>
     </div>
   );
--- src/components/(mypage)/Myanswercard.tsx
+++ src/components/(mypage)/Myanswercard.tsx
@@ -1,9 +1,18 @@
-const Mycard = () => {
+type Answer = {
+  answer_id: string;
+  answer_create_at: string;
+  answer_image: string | null;
+  answer_text: string;
+  answer_answer: string;
+  answer_user_id: string;
+};
+
+const Mycard = (answer: Answer) => {
   return (
     <div className="flex flex-row m-10 w-80% justify-center text-center">
       <div className="flex-1">
-        <div className="text-xl font-bold">제목자리</div>
-        <div>내용자리</div>
+        <div className="text-xl font-bold">{answer.answer_text}</div>
+        <div>{answer.answer_answer}</div>
         <div className="flex flex-row gap-3 justify-left ml-5">
           <button className="border-2 border-orange-500 rounded-l p-1">
             수정
@@ -13,7 +22,14 @@ const Mycard = () => {
           </button>
         </div>
       </div>
-      <div className="border-2 border-white rounded-l flex-2">이미지 자리</div>
+      {!answer.answer_image ? (
+        <div className="flex-2"></div>
+      ) : (
+        <div className="border-2 border-white rounded-l flex-2">
+          이미지 자리
+        </div>
+      )}
+
       <div className="flex-5">화살표 자리</div>
     </div>
   );
