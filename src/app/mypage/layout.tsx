37036025fd7bf83768da66eaba3b9b15da38330b
usuny0317 <usuny0317@naver.com>
2025-03-24 20:02:20 +0900

    🐛 버그

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -2,16 +2,61 @@
 
 import MypageHeader from "@/components/(mypage)/mypageHeader";
 import { ReactNode, useState } from "react";
+import supabase from "../supabase/client";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  //수정 클릭
+  //수정state
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [newNickName, setNewNickName] = useState<string>("");
+  const [canChange, setCanChange] = useState<boolean>(false);
 
   //주스탄드 쓸 예정
-  const user = {
+  let user = {
     id: "6945ddb3-1281-4b59-b16c-f238709e37a9",
     nickname: "수수수",
   };
+
+  //중복 확인//타입 넣기
+  const checkNickname = async () => {
+    const { data: users, error } = await supabase
+      .from("users")
+      .select("user_nickname");
+    if (error) {
+      console.log("오류남");
+    } else {
+      const isExist = users.some((user) => user.user_nickname === newNickName);
+
+      if (isExist) {
+        alert("이미 존재합니다!");
+      } else {
+        alert("추가할 수 있습니다~!");
+        setCanChange(true);
+      }
+    }
+  };
+
+  //수정하기
+  const changeNickname = async (e) => {
+    e.preventDefault();
+    if (canChange) {
+      const { data, error } = await supabase
+        .from("users")
+        .update({ user_nickname: newNickName })
+        .eq("user_id", user.id);
+      //.select("*");
+      console.log("data:", data);
+      console.log("error:", error);
+      if (error) {
+        //alert(`${error}`);
+      } else {
+        //alert(" " + data[0]);
+        console.log(data);
+      }
+    } else {
+      alert("중복검사를 먼저 해주세요!");
+    }
+  };
+
   return (
     <div className="p-10 gap-4">
       <div className="flex flex-row">
@@ -23,26 +68,39 @@ const Layout = ({ children }: { children: ReactNode }) => {
           >
             수정아이콘
           </div>
-          <div
+          <form
+            onSubmit={changeNickname}
             className={`${
               isVisible ? "block" : "hidden"
             } flex flex-row p-1 bg-gray-200 rounded-md gap-1`}
           >
-            <input />
-            <button className="border-2 border-orange-500 bg-orange-500 rounded ">
+            <input
+              value={newNickName}
+              onChange={(e) => setNewNickName(e.target.value)}
+            />
+            <button
+              className="border-2 border-orange-500 bg-orange-500 rounded "
+              type="button"
+              onClick={() => {
+                checkNickname();
+              }}
+            >
               중복검사
             </button>
-            <button className="border-2 border-orange-500 bg-orange-500 rounded ">
+            <button
+              className="border-2 border-orange-500 bg-orange-500 rounded "
+              type="submit"
+            >
               수정
             </button>
-          </div>
+          </form>
         </div>
       </div>
       <div className="bg-gray-700 m-8 py-10 text-xl text-white rounded-xl">
-        <p className="pl-5 flex flex-row">
+        <div className="pl-5 flex flex-row">
           <p className="text-orange-500 font-bold pr-1">{user.nickname}님</p>의
           질문과 답변을 모아보고 복습해보세요!
-        </p>
+        </div>
       </div>
       <MypageHeader />
       <div>{children}</div>
