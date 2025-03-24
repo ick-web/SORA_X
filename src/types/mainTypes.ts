c3cc78fb52cc6c339d78f3dc991d5a2f06e3ae15
mzsoba <owt4832@gmail.com>
2025-03-24 14:17:47 +0900

    ✨ feat/ mainpage에서 사용하는 기타 타입 추가

--- src/types/mainTypes.ts
+++ src/types/mainTypes.ts
@@ -1,4 +1,19 @@
 export interface answerPropsType {
   isLoading: boolean;
   answer: string;
+  question: string;
+}
+
+export type MessageContent =
+  | { type: "text"; text: string }
+  | { type: "image_url"; image_url: { url: string } };
+
+export interface MessageTypes {
+  role: "system" | "user";
+  content: string | MessageContent[];
+}
+
+export interface RequestData {
+  question: string;
+  image_url?: string;
 }
