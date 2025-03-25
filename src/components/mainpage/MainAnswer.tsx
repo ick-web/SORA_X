0710382a0a6be294ddbbac7584a1f5ae91d716c3
mzsoba <owt4832@gmail.com>
2025-03-25 14:46:27 +0900

    ✨ feat/ 답변 컴포넌트 ui 변경(불필요한 여백 제거)

--- src/components/mainpage/MainAnswer.tsx
+++ src/components/mainpage/MainAnswer.tsx
@@ -18,16 +18,20 @@ const MainAnswer = ({
 
       {!isLoading && answer && (
         <div className="w-full max-w-2xl mt-2 p-4 bg-[#2a2a2a] rounded-lg text-white whitespace-pre-line">
-          <Image
-            src={imageUrl || ""}
-            alt="미리보기"
-            className="max-h-32 max-w-full object-contain"
-            width={600}
-            height={600}
-          />
-          {`
+          {imageUrl && (
+            <>
+              <Image
+                src={imageUrl || ""}
+                alt="미리보기"
+                className="max-h-32 max-w-full object-contain"
+                width={600}
+                height={600}
+              />
+              {`
             
-            `}
+              `}
+            </>
+          )}
           질문: {question}
           {`
             
