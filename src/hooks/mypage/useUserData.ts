a4cb5e174741d43b6dbe9757a380c4ffda1b1e38
usuny0317 <usuny0317@naver.com>
2025-03-25 16:07:28 +0900

    ✨ feat/ 텐스텍으로 유저 게시글 정보 가져오기

--- src/hooks/mypage/useUserData.ts
+++ src/hooks/mypage/useUserData.ts
@@ -6,6 +6,7 @@ const SUPABASE_TABLE_NAME = {
   COMMENTS: "comments",
 };
 
+//유저 게시글 불러오기
 const getuserpost = async (userid: string) => {
   const { data, error } = await supabase
     .from(SUPABASE_TABLE_NAME.ANSWER)
@@ -19,8 +20,29 @@ const getuserpost = async (userid: string) => {
 
 export const usePostData = (userId: string) => {
   return useQuery({
-    queryKey: ["user", userId],
+    queryKey: ["answers", userId],
     queryFn: () => getuserpost(userId),
+    staleTime: 1000 * 60 * 5, // 5분 동안 캐싱 유지
+  });
+};
+
+//유저 댓글 불러오기
+
+const getusercomment = async (userid: string) => {
+  const { data, error } = await supabase
+    .from(SUPABASE_TABLE_NAME.COMMENTS)
+    .select(`*`)
+    .eq("comment_user_id", userid);
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const useCommentData = (userId: string) => {
+  return useQuery({
+    queryKey: ["comments", userId],
+    queryFn: () => getusercomment(userId),
     staleTime: 1000 * 60 * 5, // 5분 동안 캐싱 유지
   });
 };
