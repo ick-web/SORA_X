4e1d494827ae631489637c6864a5cc41d254f2eb
siheon <rlatlgjs357@gmail.com>
2025-03-25 14:11:05 +0900

    feat: 질문 데이터에 이미지 있을 경우 표시

--- src/components/(detail)/QuestionDetail.tsx
+++ src/components/(detail)/QuestionDetail.tsx
@@ -2,7 +2,7 @@ import { fetchAnswerById } from "@/utils/detail/fetchAnswerById";
 import Image from "next/image";
 
 const QuestionDetail = async () => {
-  const answerId = "db085cbd-2f76-4312-9804-90eeb2499558"; // 임시로 answer_id 넣음
+  const answerId = "d61751e4-931a-4779-9d97-66654be5da2f"; // 임시로 answer_id 넣음
   const data = await fetchAnswerById(answerId);
 
   if (!data) return <p>데이터를 불러오지 못했습니다.</p>;
@@ -11,17 +11,16 @@ const QuestionDetail = async () => {
     <div className="p-4 mb-10 border-b border-color-black3">
       <h2 className="text-2xl font-bold mb-4">질문</h2>
       <div className="bg-color-black2 mb-6 rounded p-2">
-        {data.answer_image ? (
+        {data.answer_image && (
           <Image
             src={data.answer_image}
             alt="User question"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
             className="mb-6"
           />
-        ) : (
-          <p>{data.answer_text}</p>
         )}
+        {data.answer_text && <p>{data.answer_text}</p>}
       </div>
       <h2 className="text-2xl font-bold mb-4">풀이</h2>
       <p className="bg-color-black2 mb-6 rounded p-2">{data.answer_answer}</p>
