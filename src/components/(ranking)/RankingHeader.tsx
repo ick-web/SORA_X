5c8899085f34ec221129c092b00b23eb2351eb29
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-26 16:41:43 +0900

    ✨ feat/ 랭킹 헤더 스타일 개선 및 애니메이션 조정

--- src/components/(ranking)/RankingHeader.tsx
+++ src/components/(ranking)/RankingHeader.tsx
@@ -8,22 +8,22 @@ const RankingHeader = () => {
   const pathname = usePathname();
 
   return (
-    <div className="relative flex gap-4 text-xl font-bold text-white">
-      <Link href="/ranking" className="relative py-2">
+    <div className="relative flex gap-4 text-xl font-medium text-white ">
+      <Link href="/ranking" className="relative py-2 focus:text-color-orange1">
         질문 랭킹
       </Link>
-      <Link href="/ranking/comment-rank" className="relative py-2">
+      <Link href="/ranking/comment-rank" className="relative py-2 focus:text-color-orange1">
         댓글 랭킹
       </Link>
       <motion.div
-        className="absolute bottom-0 h-1 bg-white"
+        className="absolute bottom-0 h-1 bg-color-orange1"
         layoutId="underline"
         initial={false}
         animate={{
           width: pathname === "/ranking" ? "80px" : "80px", // 탭 크기 조절
           x: pathname === "/ranking" ? "0%" : "118%", // 왼쪽 <-->오른쪽 이동
         }}
-        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+        transition={{ type: "spring", stiffness: 400, damping: 30, ease: "easeIn" }}
       />
     </div>
   );
