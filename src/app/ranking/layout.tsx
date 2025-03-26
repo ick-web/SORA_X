e6e99d07f6b7e209ef63cdb338c880abafe5a766
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-26 15:44:55 +0900

    ✨ feat/ 랭킹 레이아웃 및 헤더 스타일 개선

--- src/app/ranking/layout.tsx
+++ src/app/ranking/layout.tsx
@@ -1,15 +1,21 @@
 import RankingHeader from "@/components/(ranking)/RankingHeader";
 import RefreshButton from "@/components/(ranking)/RefreshButton";
 import { ReactNode } from "react";
+import { HiMiniTrophy } from "react-icons/hi2";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex justify-center m-16">
-      <main className="flex flex-col justify-center items-start gap-8">
-        <h1 className="text-4xl font-bold">Ranking</h1>
-        <div className="w-full flex flex-row justify-start items-center gap-4">
-          <RankingHeader />
-          <RefreshButton />
+    <div className="h-full bg-color-black1 flex flex-col items-center">
+      <main>
+        <div className="w-full flex flex-col mt-16 mb-8">
+          <h1 className="flex justify-start text-4xl font-bold mb-8 gap-2">
+            <HiMiniTrophy />
+            학습 랭크
+          </h1>
+          <div className="flex flex-row justify-start items-center gap-4">
+            <RankingHeader />
+            <RefreshButton />
+          </div>
         </div>
         <div>{children}</div>
       </main>
