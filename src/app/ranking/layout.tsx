8b737c89492ce01e744c5db6e4bab47821e9bca7
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-26 16:04:42 +0900

    ✨ feat/ 랭킹 레이아웃 및 댓글 순위 스타일 개선

--- src/app/ranking/layout.tsx
+++ src/app/ranking/layout.tsx
@@ -5,9 +5,9 @@ import { HiMiniTrophy } from "react-icons/hi2";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="h-full bg-color-black1 flex flex-col items-center">
+    <div className="h-full bg-color-black1 flex flex-col items-center pt-24">
       <main>
-        <div className="w-full flex flex-col mt-16 mb-8">
+        <div className="w-full flex flex-col mb-10">
           <h1 className="flex justify-start text-4xl font-bold mb-8 gap-2">
             <HiMiniTrophy />
             학습 랭크
