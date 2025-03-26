26207698e88cd651550e8ad4bf79f6ce139592e6
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-26 16:53:12 +0900

    ✨ feat/ 댓글 및 질문 순위 컴포넌트에서 텍스트 정렬 개선

--- src/components/(ranking)/CommentRank.tsx
+++ src/components/(ranking)/CommentRank.tsx
@@ -19,7 +19,7 @@ const CommentRank = ({ commentUsers }: { commentUsers: CommentUsersResponse[] | 
               {commentCount[i] ? `${count} 등` : ""}
             </p>
             <p className="w-56 text-xl mr-96">{userNickname[i]}</p>
-            <p className="w-16 text-xl text-right">{commentCount[i] ? `${commentCount[i]}회` : ""}</p>
+            <p className="w-16 flex-1 text-xl text-right">{commentCount[i] ? `${commentCount[i]}회` : ""}</p>
           </li>
         );
       })}
